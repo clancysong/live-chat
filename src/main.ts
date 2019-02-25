cd9c928c85d24a87fe81cf3f44389f85da26ff38
clancysong <clancysong@gmail.com>
2019-02-25 17:21:59 +0800

    Update code
    1. 引入 axios 和 element-ui 依赖，自定义 element-ui 主题色
    2. 新建 vue.config.js 文件，添加反向代理服务配置
    3. 新增 Login 页面组件与相关路由配置

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './styles/element-variables.scss'
+
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 new Vue({
   router,
---
+++ src/views/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="login">
+    <el-button type="primary" round @click="getUser">获取 id 为 1 的用户</el-button>
+    <el-button type="primary" round @click="login">登录</el-button>
+    <el-button type="primary" round @click="register">注册</el-button>
+    <el-button type="primary" round @click="logout">注销</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import { Button } from 'element-ui'
+import axios from 'axios'
+
+@Component
+export default class Login extends Vue {
+  private URL = '/api/v1'
+
+  private async getUser() {
+    const res = await axios.get(`${this.URL}/user/1`, { withCredentials: true })
+    console.log(res.data)
+  }
+
+  private async login() {
+    const fd = new FormData()
+
+    fd.append('username', 'user1')
+    fd.append('password', 'uu')
+
+    const res = await axios.post(`${this.URL}/auth/login`, fd)
+    console.log(res.data)
+  }
+
+  private async register() {
+    const fd = new FormData()
+
+    fd.append('username', 'test15')
+    fd.append('password', 'test')
+
+    const res = await axios.post(`${this.URL}/auth/register`, fd)
+    console.log(res.data)
+  }
+
+  private async logout() {
+    const res = await axios.delete(`${this.URL}/auth/logout`)
+    console.log(res.data)
+  }
+}
+</script>

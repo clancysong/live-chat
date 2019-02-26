c9c3b25426f72ece2505fbd78b5dafb726081bdb
clancysong <clancysong@gmail.com>
2019-02-26 11:21:44 +0800

    Add element and fontawesome plugins

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import Vue from 'vue'
-import ElementUI from 'element-ui'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import './styles/element-variables.scss'
+import './plugins/element'
+import './plugins/fontawesome'
 
 Vue.config.productionTip = false
-
-Vue.use(ElementUI)
 
 new Vue({
   router,

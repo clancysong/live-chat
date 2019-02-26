c426e9952008deead4ee25e18000e8943a5918c3
clancysong <clancysong@gmail.com>
2019-02-26 17:08:32 +0800

    Update code
    1. fontawesome 图标添加 brands 和 regular 分类
    2. 引入 normalize.scss 样式初始化文件
    3. 添加 LoginForm 组件，完成组件样式
    4. 为 Login 页面组件添加内容与样式

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,49 +1,77 @@
 <template>
   <div class="login">
-    <el-button type="primary" round @click="getUser">获取 id 为 1 的用户</el-button>
-    <el-button type="primary" round @click="login">登录</el-button>
-    <el-button type="primary" round @click="register">注册</el-button>
-    <el-button type="primary" round @click="logout">注销</el-button>
+    <main>
+      <login-form/>
+      <div class="side">
+        <h1>Hello, Friend!</h1>
+        <p>Enter your personnel details and start journey with us</p>
+        <el-button type="primary" round>SIGN UP</el-button>
+      </div>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
-import { Button } from 'element-ui'
-import axios from 'axios'
+import LoginForm from '@/components/LoginForm.vue'
 
-@Component
-export default class Login extends Vue {
-  private URL = '/api/v1'
+@Component({ components: { LoginForm } })
+export default class Login extends Vue {}
+</script>
 
-  private async getUser() {
-    const res = await axios.get(`${this.URL}/user/1`, { withCredentials: true })
-    console.log(res.data)
+<style lang="scss" scoped>
+.login {
+  $main-width: 1000px;
+
+  @mixin flex-center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
-  private async login() {
-    const fd = new FormData()
+  height: 100vh;
+  background: #f0f4f3;
+  @include flex-center();
 
-    fd.append('username', 'user1')
-    fd.append('password', 'uu')
+  > main {
+    min-width: $main-width;
+    min-height: $main-width / 1.6;
+    background: #fff;
+    border-radius: 12px;
+    box-shadow: -12px 16px 2px 0px rgba(204, 204, 204, 0.24);
+    overflow: hidden;
+    display: flex;
 
-    const res = await axios.post(`${this.URL}/auth/login`, fd)
-    console.log(res.data)
-  }
+    .login-form {
+      flex: 0 0 62.2%;
+    }
 
-  private async register() {
-    const fd = new FormData()
+    .side {
+      flex: 0 0 37.8%;
+      background: #3bb19b;
+      color: #fff;
+      padding: 0 84px;
+      @include flex-center();
+      flex-direction: column;
 
-    fd.append('username', 'test15')
-    fd.append('password', 'test')
+      > h1 {
+        font-weight: 900;
+        margin: 0;
+      }
 
-    const res = await axios.post(`${this.URL}/auth/register`, fd)
-    console.log(res.data)
-  }
+      > p {
+        margin: 24px 0 48px;
+      }
 
-  private async logout() {
-    const res = await axios.delete(`${this.URL}/auth/logout`)
-    console.log(res.data)
+      .el-button {
+        outline: none;
+        padding-left: 64px;
+        padding-right: 64px;
+        letter-spacing: 2px;
+        border: 1px solid #fff;
+      }
+    }
   }
 }
-</script>
+</style>
+

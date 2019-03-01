a6a39f791842e295f3ca7b8c8c33820f360f4e43
clancysong <clancysong@gmail.com>
2019-03-01 10:29:19 +0800

    Update code
    1. 项目整体需求改动：移除 Phaser 依赖及所有相关文件
    2. 引入 socket.io-client 依赖及其示例代码
    3. 项目更名为 live-chat

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="home">
-    <Game/>
+    <el-button @click="sendMessage">发送</el-button>
     <el-button @click="logout">注销</el-button>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import io from '@/utils/socket'
 import axios from 'axios'
 import router from '@/router'
-import Game from '@/components/Game.vue'
 
-@Component({ components: { Game } })
+@Component
 export default class Home extends Vue {
   private beforeCreate() {
-    axios.get('/api/v1/auth')
+    // axios.get('/api/v1/auth')
+  }
+
+  private sendMessage() {
+    io.emit('test', 'hello')
   }
 
   private logout() {
